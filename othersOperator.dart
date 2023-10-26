8f2a4e64bc1baf6a87d5c7fb13a65b98e8448c71
RlM100always <rakib100rlms@gmail.com>
2023-10-26 11:49:21 +0600

    OthersOperator

---
+++ othersOperator.dart
@@ -0,0 +1,22 @@
+void main() {
+  // var n1 = 10;
+  // var n2 = 5;
+
+  // print("n1+n2 = $n1 *** $n2 **** ${n1 + n2}");
+
+  var x = 30;
+  print(x++); //30
+  print(x); //31
+
+  var y = 25;
+  print(++y); //26
+  print(y); //26
+
+  var z = 10;
+  print(--z); //9
+  print(z); //9
+
+  var u = 12;
+  print(u--); //12
+  print(u); //11
+}
